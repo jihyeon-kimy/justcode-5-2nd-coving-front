9385451bc648517b84d80384d55ef5358284b43b
김민욱 <minuk3508@gmail.com>
2022-07-11 17:32:14 +0900

    modify: 파일추가

---
+++ src/page/mypage/mypage.js
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+function Mypage() {
+  return <div>Mypage</div>;
+}
+
+export default Mypage;
---
+++ src/page/opening/opening.js
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+function Opening() {
+  return <div>Opening</div>;
+}
+
+export default Opening;
