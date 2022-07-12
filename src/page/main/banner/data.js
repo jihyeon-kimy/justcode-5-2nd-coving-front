6ea0c9a012ba7777fc50b8fe73cd5d3e3d41c0a9
jihyeon-kimy <jihyeon.rem@gmail.com>
2022-07-13 08:42:01 +0900

    Add: 배너 Mock Data 추가

---
+++ src/page/main/banner/data.js
@@ -0,0 +1,22 @@
+const SLIDER_LIST = [
+  {
+    title: '',
+    subTitle: '박정민X임윤아의 가슴 따뜻한 이야기',
+    img: 'https://image.tving.com/upload/fe/highlight/2022/0712/20220712014540banner_image_url_u.jpg/dims/resize/F_webp,1920',
+    programId: 1,
+  },
+  {
+    title: `다른 사람들이 많이 보는 영화`,
+    subTitle: '<북 오브 러브>, <스텔라>등',
+    img: 'https://image.tving.com/upload/fe/highlight/2022/0712/20220712200606banner_image_url_u.jpg/dims/resize/F_webp,1920',
+    programId: 2,
+  },
+  {
+    title: '',
+    subTitle: '소녀시대의 탐나는 예능 정복기',
+    img: 'https://image.tving.com/upload/fe/highlight/2022/0705/20220705191619banner_image_url_u.jpg/dims/resize/F_webp,1920',
+    programId: 3,
+  },
+];
+
+export default SLIDER_LIST;
