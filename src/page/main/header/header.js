94c3520c75723ba5893b6b119d0a38a20af02432
jihyeon-kimy <jihyeon.rem@gmail.com>
2022-07-14 01:16:31 +0900

    Add: header 레이아웃

--- src/page/main/header/header.js
+++ src/page/main/header/header.js
@@ -1,7 +1,104 @@
 import styled from 'styled-components';
+import { FiSearch } from 'react-icons/fi';
 
 function Header() {
-  return <div>Header</div>;
+  return (
+    <Container>
+      <Navbar>
+        <NavLeft>
+          <Logo />
+          <Menu>TV프로그램</Menu>
+        </NavLeft>
+        <NavRight>
+          <FiSearch className="SearchIcon" />
+          <DropdownMenu />
+        </NavRight>
+      </Navbar>
+    </Container>
+  );
 }
 
 export default Header;
+
+const Container = styled.div`
+  width: 100%;
+  position: fixed;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-end;
+  z-index: 2;
+`;
+
+const Navbar = styled.div`
+  width: 100%;
+  height: 6%;
+  padding: 0 5%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  button {
+    background: transparent;
+    padding: 0;
+    border: 0;
+    outline: 0;
+  }
+`;
+
+const NavLeft = styled.div`
+  width: 20%;
+  min-width: 130px;
+  display: flex;
+  justify-content: space-between;
+`;
+
+const Logo = styled.img.attrs(props => ({
+  src: './image/COVING-LOGO.png',
+  alt: 'logo',
+}))`
+  width: 40%;
+  cursor: pointer;
+`;
+
+const Menu = styled.button`
+  display: flex;
+  align-items: center;
+  font-size: 1.2vw;
+  font-weight: 700;
+  color: #c4c4c4;
+  cursor: pointer;
+  :hover {
+    filter: brightness(150%);
+  }
+`;
+
+const NavRight = styled.div`
+  width: 6%;
+  min-width: 50px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  align-self: stretch;
+
+  .SearchIcon {
+    font-size: 30px;
+    cursor: pointer;
+    @media (max-width: 1200px) {
+      font-size: 18px;
+    }
+    :hover {
+      filter: brightness(150%);
+    }
+  }
+`;
+
+const DropdownMenu = styled.img.attrs(props => ({
+  src: 'https://image.tving.com/upload/profile/default.png/dims/resize/F_webp,100',
+  alt: 'dropdownMenu',
+}))`
+  width: 35%;
+  min-width: 18px;
+  object-fit: contain;
+  align-self: stretch;
+  cursor: pointer;
+`;
