c13a824d2ac0893ffa2f3950311b5589ff49e8f0
jihyeon-kimy <jihyeon.rem@gmail.com>
2022-07-17 17:09:27 +0900

    Add: 검색어 입력 여부에 따른, 컴포넌트 스위칭

--- src/page/main/searchModal/searchModal.js
+++ src/page/main/searchModal/searchModal.js
@@ -1,12 +1,23 @@
+import { useEffect, useState } from 'react';
 import styled from 'styled-components';
 import KeywordView from './keywordView';
-import SearchView from './SearhView';
+import SearchView from './searhView';
 
-function SearchModal({ keywordList, setKeywordList }) {
+function SearchModal({ keywordList, setKeywordList, keywordInput }) {
+  const [isThere, setisThere] = useState('');
+  useEffect(() => {
+    setisThere(keywordInput?.length);
+  }, [keywordInput]);
   return (
     <SearchBody>
-      <KeywordView keywordList={keywordList} setKeywordList={setKeywordList} />
-      {/* <SearchView /> */}
+      {isThere ? (
+        <SearchView keywordInput={keywordInput} />
+      ) : (
+        <KeywordView
+          keywordList={keywordList}
+          setKeywordList={setKeywordList}
+        />
+      )}
     </SearchBody>
   );
 }
