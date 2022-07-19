0cc389c2fd704e27295a57669c019e11f35e1956
jihyeon-kimy <jihyeon.rem@gmail.com>
2022-07-19 17:38:54 +0900

    임시저장

---
+++ src/page/main/buttonGroup/dropDown.js
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+function Dropdown() {
+  return <></>;
+}
+
+export default Dropdown;
--- src/page/main/main.js
+++ src/page/main/main.js
@@ -1,11 +1,13 @@
 import styled from 'styled-components';
 import MultipleSlider from '../../components/multipleSlider/multipleSlider';
 import Banner from './banner/banner';
+import ButtonGroup from './buttonGroup/buttonGroup';
 
 function Main() {
   return (
     <Container>
       <Banner />
+      <ButtonGroup />
       <MultipleSlider rank={false} summary={false} />
       <MultipleSlider rank={true} summary={false} />
       <MultipleSlider rank={false} summary={true} />
