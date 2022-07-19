294429bf3863af37270e3dcbc88d3d1aac55627e
jihyeon-kimy <jihyeon.rem@gmail.com>
2022-07-19 18:35:42 +0900

    Delete: 임시저장 내역 삭제

--- src/page/main/buttonGroup/dropDown.js
+++
@@ -1,7 +0,0 @@
-import styled from 'styled-components';
-
-function Dropdown() {
-  return <></>;
-}
-
-export default Dropdown;
--- src/page/main/main.js
+++ src/page/main/main.js
@@ -1,14 +1,11 @@
 import styled from 'styled-components';
 import MultipleSlider from '../../components/multipleSlider/multipleSlider';
 import Banner from './banner/banner';
-import ButtonGroup from './buttonGroup/buttonGroup';
 
 function Main() {
   return (
     <Container>
       <Banner />
-      <ButtonGroup />
-
       <MultipleSlider rank={false} summary={false} title="기본형" />
       <MultipleSlider rank={true} summary={false} title="Rank형" />
       <MultipleSlider rank={false} summary={true} title="Summary형" />
