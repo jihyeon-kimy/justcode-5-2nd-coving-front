778999eba16fa4826e908f6647a301dc1997faee
jihyeon-kimy <jihyeon.rem@gmail.com>
2022-07-20 20:41:10 +0900

    Add: Main페이지 API연결

--- src/page/main/main.js
+++ src/page/main/main.js
@@ -1,14 +1,13 @@
 import styled from 'styled-components';
 import MultipleSlider from '../../components/multipleSlider/multipleSlider';
 import Banner from './banner/banner';
+import MainBody from './mainBody/mainbody';
 
 function Main() {
   return (
     <Container>
       <Banner />
-      <MultipleSlider rank={false} summary={false} title="기본형" />
-      <MultipleSlider rank={true} summary={false} title="Rank형" />
-      <MultipleSlider rank={false} summary={true} title="Summary형" />
+      <MainBody />
     </Container>
   );
 }
